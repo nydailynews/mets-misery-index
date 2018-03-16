a9e93290e77eca1f20a19ad99c4402dd54d19b81
freejoe76 <jmurphy@denverpost.com>
2018-03-16 17:56:32 -0400

    Heck yeah, got the twitter part of the color commentary working

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,8 @@
 // These objects handle most of the interaction on the page.
 
 // UTILS
-var util = {
-    get_json: function(path) {
+var utils = {
+   get_json: function(path, obj, callback) {
         // Downloads local json and returns it.
         // Cribbed from http://youmightnotneedjquery.com/
         var request = new XMLHttpRequest();
@@ -10,7 +10,8 @@ var util = {
 
         request.onload = function() {
             if ( request.status >= 200 && request.status < 400 ) {
-                return Json.parse(request.responseText);
+                obj.data = JSON.parse(request.responseText);
+				callback();
             }
             else {
                 return false;
@@ -18,17 +19,40 @@ var util = {
         };
         request.onerror = function() {};
         request.send();
-    }
+    },
+	add_js: function(src, callback) {
+        var s = document.createElement('script');
+        s.onload = function() { callback(); }
+        s.setAttribute('src', src);
+        document.getElementsByTagName('head')[0].appendChild(s);
+    },
 }
 
 // COLOR COMMENTARY
+// First init fires, then on_load.
 var commentary = {
+	load_quote: function(record) {},
+	load_tweet: function(record) {
+		// Given a tweet url, populate the color commentary section and load the twitter javascript.
+		console.log(this);
+		document.getElementById('tweet-link').setAttribute('href', record['twitter-url']);
+		utils.add_js('https://platform.twitter.com/widgets.js', {});
+	},
+	on_load: function() {
+		console.log('wheee');
+		// The latest item in this.data is the one we put on the page.
+		var latest = commentary.data.pop();
+		if ( latest['twitter-url'] == '' ) commentary.load_quote(latest);
+		else commentary.load_tweet(latest);
+	},
     init: function() {
-        this.data = utils.get_json('../output/mets-commentary-2018.json');
+        this.data = utils.get_json('output/mets-commentary-2018.json', commentary, this.on_load);
     }
 }
+commentary.init();
 
 // MISERY INDEX
+// First init fires, then on_load.
 var misery = {
     init: function() {
         this.data = utils.get_json('../output/mets-misery-2018.json');
@@ -36,6 +60,7 @@ var misery = {
 }
 
 // INJURY TRACKER
+// First init fires, then on_load.
 var injuries = {
     init: function() {
         this.data = utils.get_json('../output/mets-injuries-2018.json');
