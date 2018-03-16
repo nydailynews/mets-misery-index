b895dc821341289e363ca4720d5e82f724e5dc98
freejoe76 <jmurphy@denverpost.com>
2018-03-16 17:19:18 -0400

    Flesh out the main and the args handling

--- app/publish.py
+++ app/publish.py
@@ -29,3 +29,31 @@ class Misery:
     def __init__(self, sheet):
         self.sheet = sheet
 
+def main(args):
+    """ Handle the command line.
+        """
+    sheet = Sheet('NYDN Sports', args.sheet)
+    sheet.options = args
+
+def build_parser(args):
+    """ Handle the argparse and make it testable.
+        >>> args = build_parser(['--verbose'])
+        >>> print(args.verbose)
+        True
+        """
+    parser = argparse.ArgumentParser(usage = '$ python publish.py --sheet mets-misery-2018',
+                                        description='''Downloads, filters and re-publishes a Google Sheet tab.'''
+                                        epilog='')
+    parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true')
+    parser.add_argument('-t', '--test', dest='test', default=False, action='store_true')
+    parser.add_argument('-s', '--sheet', dest='sheet', default=None)
+    args = parser.parse_args(args)
+    return args
+
+if __name__ == '__main__':
+    args = build_parser(sys.argv[1:])
+
+    if args.test == True:
+        doctest.testmod(verbose=args.verbose)
+
+    main(args)
