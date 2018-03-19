a938b20c223ab0e9f7e9611948995b9baab7a39d
freejoe76 <jmurphy@denverpost.com>
2018-03-19 17:57:09 -0400

    Add some more utils

--- www/js/app.js
+++ www/js/app.js
@@ -4,12 +4,36 @@
 var utils = {
     ap_numerals: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'],
     months: ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'],
+    ap_months: ['Jan.', 'Feb.', 'March', 'April', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'],
+    ap_date: function(date) {
+        // Given a date such as "2018-02-03" return an AP style date, sans year.
+        var parts = date.split('-')
+        var day = +parts[2];
+        var month = this.ap_months[+parts[1] - 1];
+        return month + ' ' + day;
+    },
     rando: function() {
         var text = "";
         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         for ( var i=0; i < 8; i++ )
             text += possible.charAt(Math.floor(Math.random() * possible.length));
         return text;
+    },
+    add_zero: function(i) {
+        // For values less than 10, return a zero-prefixed version of that value.
+        if ( +i < 10 ) return "0" + i;
+        return i;
+    },
+    parse_date: function(date ) {
+        // date is a datetime-looking string such as "2017-07-25"
+        // Returns a unixtime integer.
+        if ( typeof date !== 'string' ) return Date.now();
+
+        var date_bits = date.split(' ')[0].split('-');
+
+        // We do that "+date_bits[1] - 1" because months are zero-indexed.
+        var d = new Date(date_bits[0], +date_bits[1] - 1, date_bits[2], 0, 0, 0);
+        return d.getTime();
     },
     get_json: function(path, obj, callback) {
         // Downloads local json and returns it.
