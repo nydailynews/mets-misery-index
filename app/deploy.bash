e4b9a84bc975b18d19aac401965e6aff43c44314
freejoe76 <jmurphy@denverpost.com>
2018-03-23 12:12:15 -0400

    Update deploy

--- app/deploy.bash
+++ app/deploy.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #workon MISERY
-declare -a SHEETS=('mets-misery-2018' 'mets-commentary-2018' 'mets-injuries-days-2018' 'mets-injured-list-2018')
+declare -a SHEETS=('mets-misery-2018' 'mets-commentary-2018' 'mets-injured-list-2018')
 for SHEET in ${SHEETS[@]}; do
     python publish.py --sheet=$SHEET
 done
