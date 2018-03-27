865f56122fa15e6fc41cb93485ccccf0a059e635
Joe Murphy <joe.murphy@gmail.com>
2018-03-27 19:19:33 +0000

    tables.sql edited online with Bitbucket

--- sql/tables.sql
+++ sql/tables.sql
@@ -4,15 +4,15 @@ CREATE TABLE `misery_votes` (
       `ip` varchar(30) COLLATE utf8_bin NOT NULL,
       `year` int(11) NOT NULL DEFAULT 2018,
       `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      PRIMARY KEY (`id`),
+      PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
 CREATE TABLE `misery_days` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
-      `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      `date` date NOT NULL,
       `year` int(11) NOT NULL DEFAULT 2018,
       `score` float NOT NULL,
       `votes` int(11) NOT NULL,
-      PRIMARY KEY (`id`),
+      PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
