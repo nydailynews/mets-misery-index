cfde2f6312a9465c02557b30437b8050f185991e
freejoe76 <jmurphy@denverpost.com>
2018-03-28 18:04:51 -0400

    Hook up the js to the gauge and the text and the banner

--- app/deploy.bash
+++ app/deploy.bash
@@ -2,7 +2,7 @@
 #workon MISERY
 declare -a SHEETS=('mets-misery-2018' 'mets-commentary-2018' 'mets-injured-list-2018')
 for SHEET in ${SHEETS[@]}; do
-    python publish.py --sheet=$SHEET
+    python3 publish.py --sheet=$SHEET
 done
 
 scp output/* qa:/apps/project/misery-index/output/
