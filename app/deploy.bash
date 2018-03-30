9583d21c69289a7d41616d63251a1429575aa4fe
webadm <joe.murphy@gmail.com>
2018-03-30 10:57:56 -0400

    Update deploy

--- app/deploy.bash
+++ app/deploy.bash
@@ -1,8 +1,16 @@
 #!/bin/bash
-#workon MISERY
-declare -a SHEETS=('mets-misery-2018' 'mets-commentary-2018' 'mets-injured-list-2018')
+workon MISERY
+if [ -e source.bash ]; then source source.bash; fi
+declare -a SHEETS=('mets-misery-2018' 'mets-commentary-2018' 'mets-injured-list-2018' 'yankee-derby-2018')
 for SHEET in ${SHEETS[@]}; do
     python3 publish.py --sheet=$SHEET
 done
 
-scp output/* qa:/apps/project/misery-index/output/
+if [ `whoami` = 'webadm' ]
+then
+	scp output/mets-* prod:/apps/project/misery-index/output/
+	scp output/yankee-derby-2018.json prod:/apps/project/yankees-sluggers/output/
+else
+	scp output/mets-* qa:/apps/project/misery-index/output/
+	scp output/yankee-derby-2018.json qa:/apps/project/yankees-sluggers/output/
+fi
