493eb1ef02050c35a21a62168afda0f4d5e25e3e
webadm <joe.murphy@gmail.com>
2018-03-30 11:24:30 -0400

    Update paths

--- app/deploy.bash
+++ app/deploy.bash
@@ -8,11 +8,11 @@ done
 
 if [ `whoami` = 'webadm' ]
 then
-	cp output/mets* /apps/project/misery-tracker/output/
-	scp output/mets-* prod:/apps/project/misery-index/output/
+	cp output/mets* /apps/project/mets-misery-index/output/
+	scp output/mets-* prod:/apps/project/mets-misery-index/output/
 	cp output/yankee* /apps/project/yankees-sluggers-tracker/output/
 	scp output/yankee-derby-2018.json prod:/apps/project/yankees-sluggers-tracker/output/
 else
-	scp output/mets-* qa:/apps/project/misery-index/output/
+	scp output/mets-* qa:/apps/project/mets-misery-index/output/
 	scp output/yankee-derby-2018.json qa:/apps/project/yankees-sluggers-tracker/output/
 fi
