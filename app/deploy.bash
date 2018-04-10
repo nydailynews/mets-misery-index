1d927f83aec59d95c143340cdfc3620e70e4c9c4
freejoe76 <jmurphy@denverpost.com>
2018-04-10 14:39:30 -0400

    Add a new spreadsheet

--- app/deploy.bash
+++ app/deploy.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 workon MISERY
 if [ -e source.bash ]; then source source.bash; fi
-declare -a SHEETS=('mets-misery-2018' 'mets-commentary-2018' 'mets-injured-list-2018' 'yankee-derby-2018')
+declare -a SHEETS=('mets-misery-2018' 'mets-commentary-2018' 'mets-injured-list-2018' 'yankee-derby-2018' 'yankees-injured-list-2018')
 for SHEET in ${SHEETS[@]}; do
     python3 publish.py --sheet=$SHEET
 done
